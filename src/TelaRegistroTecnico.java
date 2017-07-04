8996f88eab91da01bb6de842a2fd9c3663f9aedd
pftupina <pftupina@gmail.com>
2017-07-04 02:04:38 -0300

    labels foram transformados em vetor, tratamento de exceção caso algum campo esteja vazio adicionado

--- src/TelaRegistroTecnico.java
+++ src/TelaRegistroTecnico.java
@@ -5,8 +5,8 @@ import javax.swing.*;
 
 public class TelaRegistroTecnico extends JFrame{
 	
-	JLabel		labelTitulo,labelNome,labelEmail,labelTelefone,labelHabilidade;
-	JTextField 	entradaNome,entradaEmail,entradaTelefone,entradaHabilidade;
+	JLabel		label[] = new JLabel[5];
+	JTextField 	entrada[] = new JTextField[4];
 	JButton 	botaoConfirma;
 	
 	public TelaRegistroTecnico(){
@@ -16,71 +16,71 @@ public class TelaRegistroTecnico extends JFrame{
 		container.setLayout(layout);
 
 		
-		//--Nome do tecnico--//
+		//--Título--//
 
-		labelTitulo = new JLabel("Entre com os dados de registro do novo tecnico:");
-		container.add(labelTitulo);
-		layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, labelTitulo, 0, SpringLayout.HORIZONTAL_CENTER, container); //Alinhado com o centro
-		layout.putConstraint(SpringLayout.NORTH, labelTitulo, 5, SpringLayout.NORTH, container); //5 pixel da borda superior
+		label[0] = new JLabel("Entre com os dados de registro do novo tecnico:");
+		container.add(label[0]);
+		layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, label[0], 0, SpringLayout.HORIZONTAL_CENTER, container); //Alinhado com o centro
+		layout.putConstraint(SpringLayout.NORTH, label[0], 5, SpringLayout.NORTH, container); //5 pixel da borda superior
 		
 		//--Nome do tecnico--//
 		
-		labelNome = new JLabel("Nome");
-		container.add(labelNome);
-		layout.putConstraint(SpringLayout.WEST, labelNome, 5, SpringLayout.WEST, container); //5 pixel da borda esquerda
-		layout.putConstraint(SpringLayout.NORTH, labelNome, 5, SpringLayout.SOUTH, labelTitulo); //5 abaixo do titulo
+		label[1] = new JLabel("Nome");
+		container.add(label[1]);
+		layout.putConstraint(SpringLayout.WEST, label[1], 5, SpringLayout.WEST, container); //5 pixel da borda esquerda
+		layout.putConstraint(SpringLayout.NORTH, label[1], 5, SpringLayout.SOUTH, label[0]); //5 abaixo do titulo
 		
-		entradaNome = new JTextField(15);
-		container.add(entradaNome);
-		layout.putConstraint(SpringLayout.WEST, entradaNome, 5, SpringLayout.EAST, labelNome);// 5 pixel a esquerda de labelNome
-		layout.putConstraint(SpringLayout.NORTH,entradaNome, 0, SpringLayout.NORTH, labelNome); // mesma posição horizontal que labelNome
+		entrada[0] = new JTextField(15);
+		container.add(entrada[0]);
+		layout.putConstraint(SpringLayout.WEST, entrada[0], 5, SpringLayout.EAST, label[1]);// 5 pixel a esquerda de labelNome
+		layout.putConstraint(SpringLayout.NORTH,entrada[0], 0, SpringLayout.NORTH, label[1]); // mesma posição horizontal que labelNome
 		
 
 		
 		//--Email de contato do tecnico--//
 		
-		labelEmail = new JLabel("Email");
-		container.add(labelEmail);
-		layout.putConstraint(SpringLayout.WEST, labelEmail, 5, SpringLayout.WEST, container); //5 pixel da borda esquerda
-		layout.putConstraint(SpringLayout.NORTH, labelEmail, 5, SpringLayout.SOUTH, labelNome); //5 pixel abaixo de labelNome
+		label[2] = new JLabel("Email");
+		container.add(label[2]);
+		layout.putConstraint(SpringLayout.WEST, label[2], 5, SpringLayout.WEST, container); //5 pixel da borda esquerda
+		layout.putConstraint(SpringLayout.NORTH, label[2], 5, SpringLayout.SOUTH, label[1]); //5 pixel abaixo de labelNome
 		
 
-		entradaEmail = new JTextField(15);
-		container.add(entradaEmail);
-		layout.putConstraint(SpringLayout.WEST, entradaEmail, 0, SpringLayout.WEST, entradaNome);// Mesma posição horizontal que entradaNome
-		layout.putConstraint(SpringLayout.NORTH, entradaEmail, 5, SpringLayout.SOUTH, labelNome);//5 pixel abaixo de labelNome
+		entrada[1] = new JTextField(15);
+		container.add(entrada[1]);
+		layout.putConstraint(SpringLayout.WEST, entrada[1], 0, SpringLayout.WEST, entrada[0]);// Mesma posição horizontal que entradaNome
+		layout.putConstraint(SpringLayout.NORTH, entrada[1], 5, SpringLayout.SOUTH, label[1]);//5 pixel abaixo de labelNome
 		
 		//--Telefone de contato do tecnico--//
 		
-		labelTelefone = new JLabel("Telefone de contato");
-		container.add(labelTelefone);
-		layout.putConstraint(SpringLayout.WEST, labelTelefone, 5, SpringLayout.EAST, entradaNome); // 5 pixels a direita de entradaNome
-		layout.putConstraint(SpringLayout.NORTH, labelTelefone, 0, SpringLayout.NORTH, labelNome); // mesma posição horizontal que labelNome
+		label[3] = new JLabel("Telefone de contato");
+		container.add(label[3]);
+		layout.putConstraint(SpringLayout.WEST, label[3], 5, SpringLayout.EAST, entrada[0]); // 5 pixels a direita de entradaNome
+		layout.putConstraint(SpringLayout.NORTH, label[3], 0, SpringLayout.NORTH, label[1]); // mesma posição horizontal que labelNome
 		
-		entradaTelefone = new JTextField(10);
-		container.add(entradaTelefone);
-		layout.putConstraint(SpringLayout.WEST, entradaTelefone, 5, SpringLayout.EAST, labelTelefone);// 5 pixels a direita de labelTelefone
-		layout.putConstraint(SpringLayout.NORTH,entradaTelefone, 0, SpringLayout.NORTH, labelNome); // mesma posição horizontal que labelNome
+		entrada[2] = new JTextField(10);
+		container.add(entrada[2]);
+		layout.putConstraint(SpringLayout.WEST, entrada[2], 5, SpringLayout.EAST, label[3]);// 5 pixels a direita de labelTelefone
+		layout.putConstraint(SpringLayout.NORTH,entrada[2], 0, SpringLayout.NORTH, label[1]); // mesma posição horizontal que labelNome
 		
 		//--Habilidade profissional do tecnico--//
 		
-		labelHabilidade = new JLabel("Hab. Profissional");
-		container.add(labelHabilidade);
-		layout.putConstraint(SpringLayout.WEST, labelHabilidade, 0, SpringLayout.WEST, labelTelefone); //Mesma posição horizontal que labelTelefone
-		layout.putConstraint(SpringLayout.NORTH, labelHabilidade, 5, SpringLayout.SOUTH, labelNome); //5 pixel abaixo de labelNome
+		label[4] = new JLabel("Hab. Profissional");
+		container.add(label[4]);
+		layout.putConstraint(SpringLayout.WEST, label[4], 0, SpringLayout.WEST, label[3]); //Mesma posição horizontal que labelTelefone
+		layout.putConstraint(SpringLayout.NORTH, label[4], 5, SpringLayout.SOUTH, label[1]); //5 pixel abaixo de labelNome
 		
 
-		entradaHabilidade = new JTextField(10);
-		container.add(entradaHabilidade);
-		layout.putConstraint(SpringLayout.WEST, entradaHabilidade, 0, SpringLayout.WEST, entradaTelefone);// Mesma posição horizontal que entradaTelefone
-		layout.putConstraint(SpringLayout.NORTH, entradaHabilidade, 5, SpringLayout.SOUTH, labelNome);//5 pixel abaixo de labelNome
+		entrada[3] = new JTextField(10);
+		container.add(entrada[3]);
+		layout.putConstraint(SpringLayout.WEST, entrada[3], 0, SpringLayout.WEST, entrada[2]);// Mesma posição horizontal que entradaTelefone
+		layout.putConstraint(SpringLayout.NORTH, entrada[3], 5, SpringLayout.SOUTH, label[1]);//5 pixel abaixo de labelNome
 
 		//--Botao de Confimação --//
 		
 		botaoConfirma = new JButton("Confirmar");
 		container.add(botaoConfirma);
 		layout.putConstraint(SpringLayout.HORIZONTAL_CENTER, botaoConfirma, 0 , SpringLayout.HORIZONTAL_CENTER, container);
-		layout.putConstraint(SpringLayout.NORTH, botaoConfirma, 10, SpringLayout.SOUTH, labelEmail);
+		layout.putConstraint(SpringLayout.NORTH, botaoConfirma, 10, SpringLayout.SOUTH, label[2]);
 		
 		//Instancia objeto que controla os eventos
 		ControlaEventoRegistroTecnico controlaEvento = new ControlaEventoRegistroTecnico();
@@ -97,34 +97,57 @@ public class TelaRegistroTecnico extends JFrame{
 		
 	}
 	
+	
+	//Metodo usado para checar se os campos de texto estão vazios antes de pegar seu conteúdo
+	private String pegaTexto(JTextField texto) throws CampoNaoPreenchidoException{
+		
+		if(texto.getText().equals(""))
+			throw new CampoNaoPreenchidoException(texto.getName());
+		
+		return texto.getText();
+	}
+	
 	//ActionListener referente ao botao de confirmacao
 	private class ControlaEventoRegistroTecnico implements ActionListener 
 	{
 		public void actionPerformed(ActionEvent evento) {
 			
 			//Strings referentes aos dados preenchidos
-			String nomeString = "";
-			String emailString = "";
-			String telefoneString = "";
-			String habilidadeString = "";
+			String resultado[] = new String[4];
+			for (int i = 0; i < resultado.length; i++) {
+				resultado[i] = "";
+			}
 			
 			//Se o botão de confirmacao é ativado
 			if(evento.getSource()== botaoConfirma){
 				//Pega o texto digitado nas caixas
-				nomeString = entradaNome.getText();
-				emailString = entradaEmail.getText();
-				telefoneString = entradaTelefone.getText();
-				habilidadeString = entradaHabilidade.getText();
+				for (int i = 0; i < resultado.length; i++) {
+					//Checa se os campos estão vazios
+					try {
+						resultado[i] = pegaTexto(entrada[i]);
+					} catch (CampoNaoPreenchidoException e) {
+						//Exibe um erro e cancela a checagem das caixas até que todos os campos tenham sido preenchidos
+						JOptionPane.showMessageDialog(null,"Todos os campos devem ser preenchidos","Erro!",JOptionPane.ERROR_MESSAGE);
+						return;
+					}
+				}
+
 				
 				//DEBUG, caixa de dialogo com os os dados dos textos.
-				JOptionPane.showMessageDialog(null, "Nome: " + nomeString + "\nEmail: "+ emailString + "\nTelefone de contato: "+ telefoneString + "\nHabilidade Profissional: "+ habilidadeString,"DEBUG",JOptionPane.INFORMATION_MESSAGE);
+				JOptionPane.showMessageDialog(null, "Nome: " + resultado[0] + "\nEmail: "+ resultado[1] + "\nTelefone de contato: "+ resultado[2] + "\nHabilidade Profissional: "+ resultado[3],"DEBUG",JOptionPane.INFORMATION_MESSAGE);
 				
 			}
 		}
 	}
 	
-	private class DadoNaoInseridoException extends Exception{
-		//public string 
+
+	//Exceção caso algum campo esteja vazio
+	private class CampoNaoPreenchidoException extends Exception{
+		public String nomeCampo;
+		public CampoNaoPreenchidoException(String campo){
+			super("Campo de nome: "+ campo + " não preenchido.");
+			nomeCampo= campo;
+		}
 	}
 }
 
